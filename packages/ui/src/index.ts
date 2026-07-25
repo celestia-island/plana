c12d2f6717b3672b24840791f2b67f5f70dc881a
伊欧 <langyo.china@gmail.com>
2026-07-25 16:34:59 +0800

    🔥 Remove GaugeCarousel (moved to hikari as HkGaugeCarousel)

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -2,7 +2,5 @@ export { PlanaAdminShell } from "./components/PlanaAdminShell";
 export { PlanaAdminHeader } from "./components/PlanaAdminHeader";
 export { PlanaStatusBar } from "./components/PlanaStatusBar";
 export { PlanaLocalePicker } from "./components/PlanaLocalePicker";
-export { default as GaugeCarousel } from "./components/GaugeCarousel";
-export type { GaugeItem } from "./components/GaugeCarousel";
 export type { LocaleOption } from "./components/PlanaAdminHeader";
 export type { PlanaConnectionInfo } from "./components/PlanaStatusBar";
